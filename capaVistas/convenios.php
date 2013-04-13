eafac7771e884c001a6ecf0b272f8b5dbe470424
Alain Gerard Eguillor Verdonck <alaineguillor@gmail.com>
2013-04-13 17:22:50 -0400

    Cambia el nombre de "Adidas".

--- capaVistas/convenios.php
+++ capaVistas/convenios.php
@@ -1,6 +1,6 @@
 ﻿<ul class="nav nav-list">
               <li class="nav-header"><h4>Convenios</h4></li>
-              <li class="active"><a href="#">Addidas</a></li>
+              <li class="active"><a href="#">Adidas</a></li>
               <li><a href="#">Brooks</a></li>
               <li><a href="#">Nike</a></li>
 			  <li><a href="#">        </a></li>
