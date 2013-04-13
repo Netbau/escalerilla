9dcb5844cdc1376fc30c35ac0820cb09830d1c18
Alain Gerard Eguillor Verdonck <alaineguillor@gmail.com>
2013-04-13 16:05:06 -0400

    Revert "Probando GitHub"
    This reverts commit 84a5b107f6d91b82d10c0e8f63a9eb62beaad340.

--- usuario.php
+++ usuario.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-<!--hola-->
+
     <!-- Le styles -->
     <link href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" rel="stylesheet"><!-- css jQuery UI-->
 	<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"><!-- css bootstrap-->
