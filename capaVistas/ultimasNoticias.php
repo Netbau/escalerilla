ebc5b49e8dd8d50a4d19effdf490185a4fbde7e3
Alain Gerard Eguillor Verdonck <alaineguillor@gmail.com>
2013-04-13 19:37:30 -0400

    Creación header
    Creación header
    Cambios en las noticias
    Espacio publicitario

--- capaVistas/ultimasNoticias.php
+++ capaVistas/ultimasNoticias.php
@@ -1,5 +1,5 @@
 ﻿
 <li class="nav-header"><h4>Últimas Noticias</h4></li>
-			<li><div><hr><h5>21-04-2013</h5></div> <div>"Ha comenzado el periodo de otoño..."</div><div class="pull-right"><a href="#"> Leer más...</a></div></li>
-			<li><div><hr><h5>25-03-2013</h5></div> <div>"Variación en el reglamento del torneo..."</div><div class="pull-right"><a href="#"> Leer más...</a></div></li>
-			<li><div><hr><h5>13-03-2013</h5></div> <div>"Se ha dado inicio al torneo..."</div><div class="pull-right"><a href="#"> Leer más...</a></div></li>
+			<li><div><hr><h5>21-04-2013</h5></div><div class="pull-right"><a href="#"> Leer más...</a></div><div>"Ha comenzado el periodo de otoño..."</div></li>
+			<li><div><hr><h5>25-03-2013</h5></div><div class="pull-right"><a href="#"> Leer más...</a></div><div>"Variación en el reglamento del torneo..."</div></li>
+			<li><div><hr><h5>13-03-2013</h5></div><div class="pull-right"><a href="#"> Leer más...</a></div><div>"Se ha dado inicio al torneo..."</div></li>
--- index.php
+++ index.php
@@ -1,55 +1,5 @@
-﻿<!DOCTYPE html>
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta charset="utf-8">
-        <title>Rama de Tenis, Estadio Israelita</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="">
-        <meta name="author" content="">
-
-        <!-- Le styles -->
-        <link href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" rel="stylesheet"><!-- css jQuery UI-->
-        <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"><!-- css bootstrap-->
-        <link href="http://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" rel="stylesheet"><!-- css bootstrap-->
-        <style type="text/css">
-
-            body {
-                padding-top: 60px;
-                padding-bottom: 40px;
-            }
-            .sidebar-nav {
-                padding: 9px 0;
-            }
-            .tab-content {
-                max-height: 22	0px;
-            }
-
-            @media (max-width: 980px) {
-                /* Enable use of floated navbar text */
-                .navbar-text.pull-right {
-                    float: none;
-                    padding-left: 5px;
-                    padding-right: 5px;
-                }
-            }
-        </style>
-        <!-- scripts js externos --> 
-        <script src="http://code.jquery.com/jquery-latest.js"></script><!-- CDN jquery-->
-        <script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script><!-- CDN jquery tools-->
-        <script src="http://code.jquery.com/ui/1.10.0/jquery-ui.js"></script><!-- CDN jquery UI-->
-        <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script><!-- CDN bootstrap-->
-
-
-    </head>
-
-    <body>
-
-        <div class="navbar navbar-inverse navbar-fixed-top">
-
-            <?php include("capaVistas/navbar.php"); ?>
-
-        </div>
+﻿	
+	<?php require("capaVistas/header.php"); ?>
 
         <div class="container-fluid">
             <div class="row-fluid"><h2><center>Escalerilla Estadio Israelita 2013 <img src="img/LogoEimCompacto.jpg" height ="35" width="40"></center></h2></div>
@@ -66,6 +16,13 @@
                         <?php require("capaVistas/convenios.php"); ?>
 
                     </div><!--/.well -->
+		  
+		  <div class="well"><!--Publicidad-->
+            
+			Espacio Publicitario
+			
+          </div><!--/.well -->
+		  
 
                 </div><!--/span-->
                 <div class="span9">
@@ -74,16 +31,24 @@
                         <?php require("capaVistas/ranking.php"); ?>
 
                     </div>
-
-                    <div class="row-fluid"><!--Últimos Ganadores-->
+					
+					<div class="well">
+                    <div class="row-fluid"><center><h3>Últimos Ganadores</h3></center><!--Últimos Ganadores-->
 
                         <?php require("capaVistas/ultimosGanadores.php"); ?>
 
                     </div><!--/row-->
+					</div><!--well-->
                 </div><!--/span-->
             </div><!--/row-->
 
-            <hr>
+	  <div class="well"><!--Últimas Noticias-->
+	  
+			<?php require("capaVistas/ultimasNoticias.php"); ?>
+		
+	  </div>
+	  
+      <hr>
 
             <footer><!-- ARCHIVO footer-->
 
