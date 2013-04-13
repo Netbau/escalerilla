b2977abdc9bd83d3ea308992a0899cfa2010c079
Cesar Gonzalez <cgonzb@gmail.com>
2013-04-13 17:35:37 -0400

    un par de cosillas

--- capaVistas/loginForm.php
+++ capaVistas/loginForm.php
@@ -1,11 +1,28 @@
-﻿<form class="form-signin">
+﻿<div class="form-signin">
     <h3 class="form-signin-heading">Conectarse</h3>
     <input type="text" class="input-block-level" placeholder="Ej: 15035156-k">
     <input type="password" class="input-block-level" placeholder="Contrase&ntilde;a">
     <label class="checkbox">
         <input type="checkbox" value="remember-me"> Recordarme
     </label>
-    <button class="btn btn-large btn-primary" type="submit">Ingresar</button>
+    <button class="btn btn-large btn-primary" id='ingresar'>Ingresar</button>
     <br>
     <a href="#">¿Olvid&oacute; su contrase&ntilde;a?</a>
-</form>
+</div>
+
+<script type='text/javascript'>
+$('#ingresar').click(function(){
+    $.ajax({
+            url: 'capaAjax/mostrar.php',
+            type: post,
+            data: 14659205 - 8,
+            success: function(output){
+    alert(output);
+    }
+
+
+
+
+    });
+});
+</script>  
--- index.php
+++ index.php
@@ -1,115 +1,99 @@
 ﻿<!DOCTYPE html>
 <html>
-  <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <title>Rama de Tenis, Estadio Israelita</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <title>Rama de Tenis, Estadio Israelita</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <!-- Le styles -->
-    <link href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" rel="stylesheet"><!-- css jQuery UI-->
-	<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"><!-- css bootstrap-->
-	<link href="http://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" rel="stylesheet"><!-- css bootstrap-->
-	<style type="text/css">
-	
-      body {
-        padding-top: 60px;
-        padding-bottom: 40px;
-      }
-      .sidebar-nav {
-        padding: 9px 0;
-      }
-	  .tab-content {
-	  max-height: 22	0px;
-	  }
+        <!-- Le styles -->
+        <link href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" rel="stylesheet"><!-- css jQuery UI-->
+        <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"><!-- css bootstrap-->
+        <link href="http://code.jquery.com/ui/1.10.0/themes/base/jquery-ui.css" rel="stylesheet"><!-- css bootstrap-->
+        <style type="text/css">
 
-      @media (max-width: 980px) {
-        /* Enable use of floated navbar text */
-        .navbar-text.pull-right {
-          float: none;
-          padding-left: 5px;
-          padding-right: 5px;
-        }
-      }
-    </style>
-	<!-- scripts js externos --> 
-	<script src="http://code.jquery.com/jquery-latest.js"></script><!-- CDN jquery-->
-	<script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script><!-- CDN jquery tools-->
-	<script src="http://code.jquery.com/ui/1.10.0/jquery-ui.js"></script><!-- CDN jquery UI-->
-	<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script><!-- CDN bootstrap-->
+            body {
+                padding-top: 60px;
+                padding-bottom: 40px;
+            }
+            .sidebar-nav {
+                padding: 9px 0;
+            }
+            .tab-content {
+                max-height: 22	0px;
+            }
 
-	
-  </head>
+            @media (max-width: 980px) {
+                /* Enable use of floated navbar text */
+                .navbar-text.pull-right {
+                    float: none;
+                    padding-left: 5px;
+                    padding-right: 5px;
+                }
+            }
+        </style>
+        <!-- scripts js externos --> 
+        <script src="http://code.jquery.com/jquery-latest.js"></script><!-- CDN jquery-->
+        <script src="http://cdn.jquerytools.org/1.2.7/full/jquery.tools.min.js"></script><!-- CDN jquery tools-->
+        <script src="http://code.jquery.com/ui/1.10.0/jquery-ui.js"></script><!-- CDN jquery UI-->
+        <script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script><!-- CDN bootstrap-->
 
-  <body>
-  
-    <div class="navbar navbar-inverse navbar-fixed-top">
-		
-		<?php include("capaVistas/navbar.php");?>
-		
+
+    </head>
+
+    <body>
+
+        <div class="navbar navbar-inverse navbar-fixed-top">
+
+            <?php include("capaVistas/navbar.php"); ?>
+
+        </div>
+
+        <div class="container-fluid">
+            <div class="row-fluid"><h2><center>ESTADIO ISRAELITA</center></h2></div>
+            <div class="row-fluid">
+                <div class="span3">
+                    <div class="well"><!--Formulario login-->
+
+                        <?php require("capaVistas/loginForm.php"); ?>
+
+                    </div><!--/.well -->
+
+                    <div class="well"><!--Convenios-->
+
+                        <?php require("capaVistas/convenios.php"); ?>
+
+                    </div><!--/.well -->
+
+                </div><!--/span-->
+                <div class="span9">
+                    <div class="hero-unit"><!--Ranking-->
+
+                        <?php require("capaVistas/ranking.php"); ?>
+
+                    </div>
+
+                    <div class="row-fluid"><!--Últimos Ganadores-->
+
+                        <?php require("capaVistas/ultimosGanadores.php"); ?>
+
+                    </div><!--/row-->
+                </div><!--/span-->
+            </div><!--/row-->
+
+            <hr>
+
+            <footer><!-- ARCHIVO footer-->
+
+                <?php require("capaVistas/footer.php"); ?>
+
+            </footer>
+
+        </div><!--/.fluid-container-->
+
     </div>
 
-     <div class="container-fluid">
-       <div class="row-fluid"><h2><center>ESTADIO ISRAELITA</center></h2></div>
-	   <div class="row-fluid">
-        <div class="span3">
-          <div class="well"><!--Formulario login-->
-             
-			 <?php require("capaVistas/loginForm.php");?>
-
-          </div><!--/.well -->
-		  
-		  <div class="well"><!--Convenios-->
-            
-			<?php require("capaVistas/convenios.php");?>
-			
-          </div><!--/.well -->
-		  
-        </div><!--/span-->
-        <div class="span9">
-          <div class="hero-unit"><!--Ranking-->
-            
-			<?php require("capaVistas/ranking.php"); ?>
-			
-          </div>
-		  
-          <div class="row-fluid"><!--Últimos Ganadores-->
-            
-			<?php require("capaVistas/ultimosGanadores.php"); ?>
-			
-          </div><!--/row-->
-        </div><!--/span-->
-      </div><!--/row-->
-
-      <hr>
-
-      <footer><!-- ARCHIVO footer-->
-        
-			<?php require("capaVistas/footer.php"); ?>
-		
-      </footer>
-
-    </div><!--/.fluid-container-->
-
-</div>
-<script>
-//    $.ajax({
-//        url: 'capaAjax/mostrar.php',
-//        type: post,
-//        data: 14659205-8,
-//        success: function(output){
-//            alert(output);
-//            
-//            
-//            
-//        }
-//        
-//        
-//        
-//        
-//    });
-</script>  
-  </body>
+</body>
 </html>
