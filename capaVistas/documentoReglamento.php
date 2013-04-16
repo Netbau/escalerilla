d03dc5fa97479b788bf955fac98c360db5694626
Alain Gerard Eguillor Verdonck <alaineguillor@gmail.com>
2013-04-16 15:03:00 -0400

    Reglamento
    Creación reglamento.php
    Creación documentoReglamento.php

---
+++ capaVistas/documentoReglamento.php
@@ -0,0 +1,63 @@
+﻿<div class="hero-unit">
+
+<h2><center>REGLAMENTO TORNEO ESCALERILLA</h2></center><br>
+
+<h4>1.- OBJETIVO</h4><br>
+	
+El objetivo del torneo de escalerilla es fomentar la práctica del tenis social y competitivo dentro de nuestra rama. 
+A través de esto, pretendemos lograr un mayor conocimiento entre los socios, interactuar en un ambiente de mayor cordialidad 
+y dinamismo dentro de la rama, y también mejorar el nivel de juego de cada uno de nosotros a través de una sana competencia.<br><br>
+
+<h4>2.- PRESENTACIÓN</h4><br>
+
+Existirá una sola escalerilla dividida en 3 categorías A, B, y C  donde podrán participar damas y varones de cualquier edad,
+que sean socios de la rama de tenis de EIM y se encuentren al día en el pago de sus cuotas sociales. Para la conformación inicial 
+de la escalerilla se tomarán en cuenta los resultados del último torneo interno de tenis y el ranking disponible a nivel nacional. 
+Los jugadores que no estén clasificados o que no hayan participado en los torneos internos, serán ubicados en la última  posición de la categoría.<br><br>
+
+<h4>3.- DURACIÓN</h4><br>
+
+La programación regular considera el inicio desde el día 01 de Marzo de cada año y hasta el 31 de Enero del año siguiente. 
+No obstante lo anterior, para dar inicio al sistema de campeonato, existirá un período especial entre el 1 de Septiembre  
+del 2012 hasta el 31 de Enero de 2013.<br><br>
+
+<h4>4.- INSCRIPCIÓN</h4><br>
+
+Todos los miembros de la Rama quedarán automáticamente inscritos en el torneo. Los socios que no deseen participar deberán 
+informarlo por escrito al coordinador de la rama Sr. Sergio Garcés o por mail a tenis@eim.cl <br><br>
+
+<h4>5.- DESAFÍOS</h4><br>
+
+Cada jugador podrá desafiar a otro en su misma categoría que se encuentre por sobre el o ella hasta 4 posiciones mas arriba de su respectivo ranking,
+por ejemplo el jugador Nº12, podría desafiar al 13, 14, 15 y 16. Si el desafiante gana ese partido ocupará el lugar del desafiado y este bajará 1 puesto.<br>
+
+Los desafíos deberán hacerse directamente entre desafiante y desafiado, solicitando a Sergio Garcés la coordinación del encuentro.
+Los partidos serán al mejor de 3 set cortos, en caso de empate, el tercer set será un tie break a 9 puntos con diferencia de 2 puntos en el score.
+Los partidos podrán realizarse cualquier día de la semana inclusive los días Sábados y Domingos con previo aviso y coordinación con el 
+Sr. Sergio Garcés quien asignará la cancha, contando éste con 2 canchas para dicha actividad. El desafiante tendrá la obligación de aportar un tarro de pelotas para dicho partido.
+El jugador desafiado tendrá un plazo de una semana para aceptar el partido y coordinar día y hora para jugarlo. 
+El desafío sólo podrá ser pospuesto en 2 oportunidades por motivos de fuerza mayor como lesiones o viajes. 
+En caso que el jugador desafiado no responda o no justifique el no aceptar un desafió, perderá por WO, y perderá su lugar en la escalerilla, en beneficio del desafiante.
+Los resultados de los desafíos  deberán ser comunicados al Sr. Sergio Garcés, y serán debidamente publicados. Asimismo, la escalerilla también estará publicada 
+con el fin de que cada jugador pueda elegir a quien desafiar.
+Solo el primer jugador de las categorías C y B tendrán derecho a desafiar al ultimo jugador de la categoría B y A respectivamente. 
+En la eventualidad de que el desafiante sea el vencedor del partido, ascenderá a la categoría superior, ocupando el último puesto de la categoría.<br><br>
+
+<h4>6.- PENALIDADES</h4><br>
+
+En caso de acordar el partido, la no presentación en cancha será sancionada como triunfo por WO, el ausente perderá su ubicación en el ranking, 
+deberá hacerle entrega de un tarro de pelotas nuevas a su contrincante y presentarle las excusas respectivas.<br><br>
+
+<h4>7.- PREMIOS</h4>
+
+Al final de cada temporada, se entregarán premios a los tres primeros lugares por categoría en la ceremonia de premiación que organiza 
+tradicionalmente la rama de tenis a fin de cada año. No obstante los premios a que se ha hecho referencia, también se otorgarán premios 
+especiales a los jugadores más entusiastas y de mejor comportamiento.<br><br>
+
+<h4>8.- TRIBUNAL DE HONOR</h4><br>
+
+Para todas aquellas materias o casos de dificultades que se presenten entre los socios de la rama, o entre estos y miembros del directorio, 
+o entre los directores que digan relación con este Reglamento, existirá un Tribunal de Honor y Mediación compuesto por los directores de la
+rama señores Jorge Urrea, Andrés Silberstein y el Sr. Alan Krausz. El Tribunal de Honor conocerá de todas las materias que este Reglamento 
+someta a su conocimiento, y resolverán al amparo de estos estatutos, teniendo presente los valores y ética de la cultura y tradiciones del pueblo judío.
+</div>
---
+++ reglamento.php
@@ -0,0 +1,47 @@
+<?php require('capaVistas/header.php');?>
+            <div class="row-fluid">
+                <div class="span3">
+                    <div class="well"><!--Formulario login-->
+
+                        <?php if(!isset($_SESSION['usuario'])){require("capaVistas/loginForm.php");}else{require("capaVistas/infoUsuario.php");} ?>
+
+                    </div><!--/.well -->
+
+                    <div class="well"><!--Convenios-->
+
+                        <?php require("capaVistas/convenios.php"); ?>
+
+                    </div><!--/.well -->
+
+                    <div class="well"><!--Publicidad-->
+
+                        Espacio Publicitario
+
+                    </div><!--/.well -->
+
+                </div><!--/span-->
+                <div class="span9">
+                    <div class="row-fluid"><!--Reglamento-->
+                        
+					<?php require("capaVistas/documentoReglamento.php"); ?>
+						
+                    </div><!--well-->
+                </div><!--/span-->
+            </div><!--/Row Fluid-->
+            <div class="well"><!--Últimas Noticias-->
+                <?php require("capaVistas/ultimasNoticias.php"); ?>
+            </div>
+
+            <hr>
+
+            <footer><!-- ARCHIVO footer-->
+
+                <?php require("capaVistas/footer.php"); ?>
+
+            </footer><!-- ARCHIVO footer-->
+
+        </div><!--/.fluid-container-->
+
+
+    </body>
+</html>
