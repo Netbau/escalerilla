3bbb6037f27a6256890d02b38f1f09d018bc423b
Cesar Gonzalez <cgonzb@gmail.com>
2013-04-20 18:12:02 -0400

    asdasd

--- dbconfig/dbconfig.php
+++ dbconfig/dbconfig.php
@@ -12,11 +12,11 @@ DEFINE ('baseDeDatos', 'BD_REMEL');
 
 */
 
-$servidor = 'netbaucl.ipagemysql.com';
-$nombre_usuario = 'escalerillas';
-$contrasena = 'papitas.69';
-$base_de_datos = 'netbau_escalerillas';
-$puerto = '3306';
+//$servidor = 'netbaucl.ipagemysql.com';
+//$nombre_usuario = 'escalerillas';
+//$contrasena = 'papitas.69';
+//$base_de_datos = 'netbau_escalerillas';
+//$puerto = '3306';
 
 
 //$servidor = 'localhost';
@@ -25,10 +25,10 @@ $puerto = '3306';
 //$base_de_datos = 'netbau_escalerillas';
 //$puerto = '3306';
 
-//$servidor = '200.54.168.133';
-//$nombre_usuario = 'root';
-//$contrasena = 'pepito.P789';
-//$base_de_datos = 'netbau_escalerillas';
-//$puerto = '3306';
+$servidor = '200.54.168.133';
+$nombre_usuario = 'root';
+$contrasena = 'pepito.P789';
+$base_de_datos = 'netbau_escalerillas';
+$puerto = '3306';
 
 ?>
