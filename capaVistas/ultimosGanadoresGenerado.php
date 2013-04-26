1c56dbf60d602e625aa0b01237d92f2964e54d18
Alain Gerard Eguillor Verdonck <alaineguillor@gmail.com>
2013-04-25 22:21:49 -0400

    Ultimos ganadores generado

---
+++ capaVistas/ultimosGanadoresGenerado.php
@@ -0,0 +1,19 @@
+﻿<center>
+    <?php
+    include_once('capaControladores/jugadores.php');
+    $ultimosGanadores = Encuentro::ultimosGanadores();
+    if (count($ultimosGanadores) != 0) {
+        foreach ($ultimosGanadores as $Ganadores) {
+            echo '<div class="span3 well well-small"><!--1ra persona-->
+        <center><img class="img img-rounded" src="' . $Ganadores['foto'] . '" height="150" width="100"></center>
+        <center><strong>' . $Ganadores['nombre'] . ' ' . $Ganadores['apellido'] . '</strong></center>
+        <center>Ranking #' . $Ganadores['ranking'] . '</center>
+    </div>';
+        }
+    } else {
+        echo '<div class="alert alert-warning"><strong>No hay ganadores.</strong>,
+		eres n&uacute;mero uno en tu categor&iacute;a. Sigue atento a los desaf&iacute;os
+		que te har&aacute;n.</div>';
+    }
+    ?>    
+</center>
