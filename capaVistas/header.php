e375e343b2a254fd1327820400bb64efb6db3cbe
Cesar Gonzalez <cgonzb@gmail.com>
2013-04-28 14:34:08 -0400

    estilos

--- capaVistas/header.php
+++ capaVistas/header.php
@@ -10,12 +10,99 @@
 
         <!-- Le styles -->
         <link href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" rel="stylesheet"><!-- css jQuery UI-->
-        <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"><!-- css bootstrap-->
-        <link href="http://netdna.bootstrapcdn.com/bootswatch/2.3.1/cerulean/bootstrap.min.css" rel="stylesheet"><!-- css bootstrap-->
-        <!--<link href="css/bootstrap.min.css" rel="stylesheet"><!--css bootstrap-->
+        <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet"><!-- css bootstrap css -->
         <!--<link href="css/bootstrap-responsive.css"><!--bootsrap responsive-->
-        <link href="css/estilo.css" rel="stylesheet"><!-- css personalizados -->
+        <link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet"><!-- font awsome -->
+        <link href="http://netdna.bootstrapcdn.com/bootswatch/2.3.1/cerulean/bootstrap.min.css" rel="stylesheet"><!-- css bootswatch-->
+        <!--<link href="css/bootstrap.min.css" rel="stylesheet"><!--css bootstrap-->
 
+        <!--<link href="css/estilo.css" rel="stylesheet"><!-- css personalizados -->
+        <style>
+            body {
+                padding-top: 40px;
+                padding-bottom: 10px;
+                background: #cedbe9;
+                /*Old browsers*/
+                background: -moz-linear-gradient(top,  #cedbe9 0%, #aac5de 17%, #6199c7 50%, #3a84c3 51%, #419ad6 59%, #4bb8f0 71%, #3a8bc2 84%, #26558b 100%);  /*FF3.6+*/
+                background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#cedbe9), color-stop(17%,#aac5de), color-stop(50%,#6199c7), color-stop(51%,#3a84c3), color-stop(59%,#419ad6), color-stop(71%,#4bb8f0), color-stop(84%,#3a8bc2), color-stop(100%,#26558b));  /*Chrome,Safari4+*/
+                background: -webkit-linear-gradient(top,  #cedbe9 0%,#aac5de 17%,#6199c7 50%,#3a84c3 51%,#419ad6 59%,#4bb8f0 71%,#3a8bc2 84%,#26558b 100%);  /*Chrome10+,Safari5.1+*/
+                background: -o-linear-gradient(top,  #cedbe9 0%,#aac5de 17%,#6199c7 50%,#3a84c3 51%,#419ad6 59%,#4bb8f0 71%,#3a8bc2 84%,#26558b 100%);  /*Opera 11.10+*/
+                background: -ms-linear-gradient(top,  #cedbe9 0%,#aac5de 17%,#6199c7 50%,#3a84c3 51%,#419ad6 59%,#4bb8f0 71%,#3a8bc2 84%,#26558b 100%);  /*IE10+*/
+                background: linear-gradient(to bottom,  #cedbe9 0%,#aac5de 17%,#6199c7 50%,#3a84c3 51%,#419ad6 59%,#4bb8f0 71%,#3a8bc2 84%,#26558b 100%);  /*W3C*/
+                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#cedbe9', endColorstr='#26558b',GradientType=0 );  /*IE6-9*/
+            }
+            .sidebar-nav {
+                padding: 5px;
+            }
+            .tab-content{
+                max-height: 238px;
+            }
+            .small{
+                max-height: 200px;
+            }
+            textarea{
+                width: 98%;
+            }
+            input{
+                width: 95%;
+            }
+            table img{
+                width: 65px;
+                height: 70px;
+
+            }
+            .span3 img{
+                width: 70px;
+                height: 70px;
+            }
+            .span6 img{
+                width: 70px;
+                height: 70px;
+            }
+
+            h1,h2,h3{
+                padding-top: 0px;
+                padding-bottom: 0px;
+                margin-top: 0px;
+                margin-bottom: 0px;
+                color: #00b2ff;
+            }
+            footer p{
+                color: #0075ff;
+            }
+
+            * {
+                margin: 0;
+                padding: 0;
+            }
+            body {
+                font: 13px/1.4 Georgia, Serif;
+            }
+            #page-wrap {
+                margin: 50px;
+            }
+            p {
+                margin: 20px 0;
+            }
+            .well {
+                padding-right: 9px;
+                padding-left: 9px;
+                padding-top: 2px;
+            }
+            .carousel{
+                height: 40px;
+            }
+            .navbar .brand{
+                padding-bottom: 0px;
+            }
+            /* iPads (portrait and landscape) ----------- */
+            @media only screen and (min-device-width: 768px) and (max-device-width: 1024px) {
+                body {
+                    width: 1024px;
+                }
+            }
+
+        </style>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script><!-- JQUERY-->
         <!--<script src="js/jquery191.js"></script><!-- JQUERY-->
         <script type="text/javascript" src="js/SimpleAjaxUploader.js"></script><!-- ajax uploader -->
@@ -66,20 +153,20 @@
         <div class="container-fluid">
             <div class="row-fluid" style="margin-top: 20px;">
 
-            <div id="myCarousel" class="carousel slide span12">
-                <!-- Carousel items -->
-                <center>
-                    <div class="carousel-inner">
-                        <div class="active item"><img src="img/logoeim.jpg"></div>
-                        <div class="item"><img src="img/slide-2.jpg"></div>
-                        <div class="item"><img src="img/slide-3.jpg"></div>
-                        <div class="item"><img src="img/slide-4.jpg"></div>
-                        <div class="item"><img src="img/slide-5.jpg"></div>
-                    </div>
+                <div id="myCarousel" class="carousel slide span12">
+                    <!-- Carousel items -->
+                    <center>
+                        <div class="carousel-inner">
+                            <div class="active item"><img src="img/logoeim.jpg"></div>
+                            <div class="item"><img src="img/slide-2.jpg"></div>
+                            <div class="item"><img src="img/slide-3.jpg"></div>
+                            <div class="item"><img src="img/slide-4.jpg"></div>
+                            <div class="item"><img src="img/slide-5.jpg"></div>
+                        </div>
 
-                    <!-- Carousel nav -->
-                    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-                    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-                </center>
-            </div>
+                        <!-- Carousel nav -->
+                        <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                        <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+                    </center>
+                </div>
             </div>
