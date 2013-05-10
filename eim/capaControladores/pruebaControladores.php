333e0698eabe68510c6b18ea832eee22eab43e4a
Cesar Gonzalez <cgonzb@gmail.com>
2013-05-10 16:09:57 -0400

    desafios bloqueados si ya desafie a alguien

--- eim/capaControladores/pruebaControladores.php
+++ eim/capaControladores/pruebaControladores.php
@@ -3,8 +3,17 @@ session_start();
 include('encuentro.php');
 include('jugadores.php');
 include('usuarios.php');
+include('desafios.php');
 print_r($_SESSION);
 echo '<hr>';
+
+//$soyDesafiador = Desafios::esDesafiador($_SESSION['jugador']['idJugadores']);
+//if($soyDesafiador){
+//    echo 'soy desafiador!!<hr>';
+//}
+//else{
+//    echo 'NO soy desafiador!!<hr>';
+//}
 
 
 $desafio = Encuentro::ultimosGanadores();
