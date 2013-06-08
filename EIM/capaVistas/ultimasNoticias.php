023e06a43c02a157ec1cb51ebb7999a02c42210f
Alain Gerard Eguillor Verdonck <alaineguillor@gmail.com>
2013-06-08 01:51:51 -0400

    Últimas Noticias

--- EIM/capaVistas/ultimasNoticias.php
+++ EIM/capaVistas/ultimasNoticias.php
@@ -1,17 +1,35 @@
 <h3>Últimas Notícias</h3>
 ﻿<div class="well well-small small" style="overflow: auto;">
     <div class="noticia">
-        <div>
-            <h5>29-04-2013 - Marcha Blanca Escalerilla</h5>
-        </div>
-        <div class="pull-right">
-            <a href="#"></a>
-        </div>
-        <div>
-            "Estimados Jugadores, En el día de hoy se da inicio a la marcha blanca del sitio. Agradecemos la participación en el evento de inscripción
-		con el que realizaremos la primera carga de información.<br>
-		Muy pronto recibirán un correo electrónico con las indicaciones necesarias para ingresar al sitio, en tanto, estaremos actualizando el ranking
-		Oficial con el señor Sergio Garcés."
-        </div>
-    </div>
+        <?php
+        require_once(dirname(__FILE__) . '/../capaControladores/noticias.php');
+        $noticias = Noticia::ultimasNoticias();
+        if (!empty($noticias)) {
+            $cantidad = 0;
+            foreach ($noticias as $noticia) {
+                echo '
+            <div class="accordion-group">
+            <div class="accordion-heading info">
+                <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse' . $noticia['idNoticias'] . '">
+                    ' . $noticia['titulo'] . '
+                </a>
+            </div>
+            <div id="collapse' . $noticia['idNoticias'] . '" class="accordion-body collapse';
+                if ($cantidad == 0) {
+                    echo ' in';
+                }
+                echo '">
+                <div class="accordion-inner">
+                    <div class="row-fluid">
+                       '. $noticia[contenido] . '
+                    </div>
+                </div>
+            </div>
+        </div><!-- collapse para premio ' . $noticia['idNoticias'] . '-->';
+                $cantidad++;
+            }
+        } else {
+            echo '<div class="alert alert-info">No Hay premios vigentes registrados.</div>';
+        }
+        ?>
 </div>
