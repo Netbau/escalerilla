40b504fca5c477850d4b911b8ddc3c0b1ba53183
Cesar Gonzalez <cgonzb@gmail.com>
2013-06-13 09:54:29 -0400

    Revert "carrousel para los proximos desafios"
    This reverts commit 9a631d4518fe7619ffcb43f75dce3ab86c5bb2eb.

--- EIM/capaVistas/proximosEncuentros.php
+++ EIM/capaVistas/proximosEncuentros.php
@@ -1,33 +1,29 @@
 <center><h3>Próximos Desafíos</h3></center>
-<div id="myCarousel" class="carousel slide span12" data-interval="1500"  style="height: 100%;">
-    <!-- Carousel items -->
-    <center>
-        <div class="carousel-inner">
-            <?php
-            include_once(dirname(__FILE__) . '/../capaControladores/encuentro.php');
-            include_once(dirname(__FILE__) . '/../capaControladores/jugadores.php');
-            $ultimosEncuentros = Encuentro::getPróximosEncuentros($limite = 3);
 
 
-            if (count($ultimosEncuentros) != 0) {
-                foreach ($ultimosEncuentros as $encuentro) {
+<div class='row-fluid'>
 
-                    $jugador1 = Jugadores::getUsuarioPorJugador($encuentro['idJugadores']);
-                    $jugador2 = Jugadores::getUsuarioPorJugador($encuentro['idJugadores1']);
+    <?php
+    include_once(dirname(__FILE__) . '/../capaControladores/encuentro.php');
+    include_once(dirname(__FILE__) . '/../capaControladores/jugadores.php');
+    $ultimosEncuentros = Encuentro::getPróximosEncuentros($limite = 3);
 
-                    $fecha = explode(' ', $encuentro['fecha']);
-                    echo '<div class="span6 offset3 item well well-small">
-                       <div class="span4"><center>' . $jugador1[0]['nombre'] . ' ' . $jugador1[0]['apellido'] . '<br><img class="img-rounded" src="' . $jugador1[0]['foto'] . '"></center></div>'
-                    . '<div class="span4"><center>' . 'vs.<br>A contar del:' . '<br>' . $fecha[0] . '</center></div>'
-                    . '<div class="span4"><center>' . $jugador2[0]['nombre'] . ' ' . $jugador2[0]['apellido'] . '<br><img class="img-rounded" src="' . $jugador2[0]['foto'] . '"></center></div>'
-                    . '</div>';
-                }
-            } else {
-                echo '<div class="alert alert-warning"><strong>No hay encuentros próximos.</strong></div>';
-            }
-            ?>
-        </div>
-    </center>
-    <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-    <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+
+    if (count($ultimosEncuentros) != 0) {
+        foreach ($ultimosEncuentros as $encuentro) {
+
+            $jugador1 = Jugadores::getUsuarioPorJugador($encuentro['idJugadores']);
+            $jugador2 = Jugadores::getUsuarioPorJugador($encuentro['idJugadores1']);
+
+            $fecha = explode(' ', $encuentro['fecha']);
+            echo '<div class="span6 well well-small"><!--1er Encuentro-->
+                <div class="span4"><center>' . $jugador1[0]['nombre'] . ' ' . $jugador1[0]['apellido'] . '<br><img class="img-rounded" src="' . $jugador1[0]['foto'] . '"></center></div>'
+            . '<div class="span4"><center>' . 'vs.<br>A contar del:' . '<br>' . $fecha[0] . '</center></div>'
+            . '<div class="span4"><center>' . $jugador2[0]['nombre'] . ' ' . $jugador2[0]['apellido'] . '<br><img class="img-rounded" src="' . $jugador2[0]['foto'] . '"></center></div>'
+            . '</div>';
+        }
+    } else {
+        echo '<div class="alert alert-warning"><strong>No hay encuentros próximos.</strong></div>';
+    }
+    ?>
 </div>
